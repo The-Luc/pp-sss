21c38ea70c288f33916c25c4cd748a3da91c04c2
Nhan Huynh <nhan.huynh@successsoftware.global>
2021-05-08 13:31:55 +0700

    PP-81-82-83: sumary real data

--- src/views/CreateBook/Manager/Summary/ImportantDates/script.js
+++ src/views/CreateBook/Manager/Summary/ImportantDates/script.js
@@ -1,1 +1,22 @@
-export default {};
+import { mapState } from 'vuex';
+import moment from 'moment';
+export default {
+  computed: {
+    ...mapState('project', ['project']),
+    dueDate() {
+      let date = moment(new Date(this.project.releaseDate)).format('MM/DD/YY');
+      return date;
+    },
+    deliveryDate() {
+      let date = moment(new Date(this.project.releaseDate))
+        .add(14, 'days')
+        .format('MM/DD/YY');
+      return date;
+    },
+    countdown(){
+      let dueDate =  moment(this.project.releaseDate, 'MM/DD/YYYY');
+      let currentDate = moment(new Date());
+      return moment(dueDate).diff(currentDate, 'days');
+    }
+  }
+};
