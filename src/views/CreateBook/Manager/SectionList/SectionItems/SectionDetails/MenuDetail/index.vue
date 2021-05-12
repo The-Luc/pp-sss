a300413cbc8f9f765e39abd0911155cf1d41033c
halik023 <liem.dang@successsoftware.global>
2021-05-12 15:15:14 +0700

    PP-104: create UI/UX menu move sheet

--- src/views/CreateBook/Manager/SectionList/SectionItems/SectionDetails/MenuDetail/index.vue
+++ src/views/CreateBook/Manager/SectionList/SectionItems/SectionDetails/MenuDetail/index.vue
@@ -1,6 +1,58 @@
 <template>
   <div class="menu-detail">
-    <Item title="Move To" value="Choose a Section"> </Item>
+    <Item
+      title="Move To"
+      value="Choose a Section"
+      @click.native="isShow = !isShow"
+    >
+      <div v-if="isShow" class="menu-move-sheet">
+        <div class="menu-move-sheet-item">
+          <div
+            class="menu-move-sheet-color"
+            :style="{ 'background-color': 'red' }"
+          ></div>
+          <div class="text-500 menu-move-sheet-title">
+            Administration & Staff
+          </div>
+        </div>
+        <div class="menu-move-sheet-item">
+          <div
+            class="menu-move-sheet-color"
+            :style="{ 'background-color': 'red' }"
+          ></div>
+          <div class="text-500 menu-move-sheet-title">
+            Administration & Staff
+          </div>
+        </div>
+        <div class="menu-move-sheet-item">
+          <div
+            class="menu-move-sheet-color"
+            :style="{ 'background-color': 'red' }"
+          ></div>
+          <div class="text-500 menu-move-sheet-title">
+            Administration & Staff
+          </div>
+        </div>
+        <div class="menu-move-sheet-item">
+          <div
+            class="menu-move-sheet-color"
+            :style="{ 'background-color': 'red' }"
+          ></div>
+          <div class="text-500 menu-move-sheet-title">
+            Administration & Staff
+          </div>
+        </div>
+        <div class="menu-move-sheet-item">
+          <div
+            class="menu-move-sheet-color"
+            :style="{ 'background-color': 'red' }"
+          ></div>
+          <div class="text-500 menu-move-sheet-title">
+            Administration & Staff
+          </div>
+        </div>
+      </div>
+    </Item>
     <slot />
   </div>
 </template>
--- src/views/CreateBook/Manager/SectionList/SectionItems/SectionDetails/MenuDetail/script.js
+++ src/views/CreateBook/Manager/SectionList/SectionItems/SectionDetails/MenuDetail/script.js
@@ -4,4 +4,15 @@ export default {
   components: {
     Item
   },
+  data() {
+    return {
+      isShow: false
+    }
+  },
+  method: {
+    onChangeMenuSheet() {
+      console.log(1);
+      // this.isShow = !this.isShow
+    }
+  }
 };
