1d10f1c68dd6d3a3470aaaf4fbc1b71910691067
Nhan Huynh <nhan.huynh@successsoftware.global>
2021-05-14 15:09:14 +0700

    PP-139: Fix style code

--- src/components/ModalEmptySection/index.vue
+++ src/components/ModalEmptySection/index.vue
@@ -4,7 +4,7 @@
       The following sections are empty:
     </div>
     <div class="box">
-      <div v-for="(section, id) in sections" :key="id">
+      <div v-for="section in sections" :key="section.id">
         {{ section.name }}
       </div>
     </div>
@@ -12,9 +12,9 @@
       Empty sections are not included in print and digital editions. Please
       either delete the empty section(s) OR add a sheet to it before proceeding.
     </div>
-    <PpButton class="pp-button" is-active @click="onChangeView()">OK</PpButton>
+    <PpButton class="pp-button" is-active @click="onChangeView">OK</PpButton>
   </Modal>
 </template>
 
 <script src="./script.js" />
-<style lang="scss" src="./style.scss">
+<style lang="scss" src="./style.scss" />
