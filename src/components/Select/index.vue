0a9a049ee3ddf37cf7ec342d258a43f1dca2457b
Koo SSS <dong.dang@successsoftware.global>
2021-05-20 17:37:47 +0700

    PP-370: Create select component

---
+++ src/components/Select/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-select
+    class="pp-select"
+    :items="items"
+    item-text="label"
+    item-value="value"
+    label="Solo field"
+    :value="items[0].value"
+    solo
+    persistent-hint
+    @change="onChange"
+  >
+    <template #append>
+      <img :src="arrowSelect" alt="arrow-select" />
+    </template>
+  </v-select>
+</template>
+
+<script src="./script.js" />
+
+<style lang="scss" src="./style.scss" />
