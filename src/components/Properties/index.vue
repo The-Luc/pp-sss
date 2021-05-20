5fb84d7f8b1e012704cd4f1e2d95f3f470c9cfc7
Koo SSS <dong.dang@successsoftware.global>
2021-05-20 15:06:21 +0700

    PP-370: Create Properties Header component

---
+++ src/components/Properties/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div :style="{ width: `${width}px` }">
+    <Header :title="title" />
+    <slot />
+  </div>
+</template>
+
+<script src="./script.js" />
+
+<style lang="scss" src="./style.scss" />
---
+++ src/components/PropertiesManager/Image/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    This is image
+  </div>
+</template>
+
+<script src="./script.js" />
+
+<style lang="scss" src="./style.scss" />
