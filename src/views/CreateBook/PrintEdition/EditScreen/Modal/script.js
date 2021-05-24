1b17698d56456cc865fc4b0ac6b23680ab580c66
halik023 <liem.dang@successsoftware.global>
2021-05-24 15:33:58 +0700

    PP122: view all yearbook theme available

--- src/views/CreateBook/PrintEdition/EditScreen/Modal/script.js
+++ src/views/CreateBook/PrintEdition/EditScreen/Modal/script.js
@@ -1,5 +1,9 @@
 import { mapGetters, mapMutations } from 'vuex';
 import { MUTATES } from '@/store/modules/app/const';
+import {
+  GETTERS as BOOK_GETTERS,
+  MUTATES as BOOK_MUTATES
+} from '@/store/modules/book/const';
 
 import Modal from '@/components/Modal';
 import PpButton from '@/components/Button';
@@ -21,13 +25,13 @@ export default {
   },
   computed: {
     ...mapGetters({
-      themes: 'book/getThemes'
+      themes: BOOK_GETTERS.GET_THEMES
     })
   },
   methods: {
     ...mapMutations({
       toggleModal: MUTATES.TOGGLE_MODAL,
-      selectTheme: 'book/selectTheme'
+      selectTheme: BOOK_MUTATES.SELECT_THEME
     }),
     /**
      * Close Modal
--- src/views/CreateBook/PrintEdition/EditScreen/SidebarSection/script.js
+++ src/views/CreateBook/PrintEdition/EditScreen/SidebarSection/script.js
@@ -2,7 +2,7 @@ import { mapGetters, mapMutations } from 'vuex';
 
 import Thumbnail from '@/components/Thumbnail/ThumbnailPrint';
 import HeaderContainer from './HeaderContainer';
-import { GETTERS } from '@/store/modules/book/const';
+import { GETTERS, MUTATES } from '@/store/modules/book/const';
 
 export default {
   components: {
@@ -17,7 +17,7 @@ export default {
   },
   methods: {
     ...mapMutations({
-      selectSheet: 'book/selectSheet'
+      selectSheet: MUTATES.SELECT_SHEET
     }),
     numberPage(sectionId, sheet) {
       const sectionIndex = this.book.sections.findIndex(
