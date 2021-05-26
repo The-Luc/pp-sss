a8dac8060bb5908d697bf4620b30298f74a65200
vuln-successsoftware <vu.luunguyen@successsoftware.global>
2021-05-26 14:06:36 +0700

    PP-219 Test color picker

--- src/components/PropertiesManager/Text/PickerPopup/index.vue
+++ src/components/PropertiesManager/Text/PickerPopup/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="color-picker-container"></div>
+  <div class="color-picker-container">
+    <photoshop-picker v-model="colors" @ok="onOk" @cancel="onCancel"></photoshop-picker>
+  </div>
 </template>
 
 <script src="./script.js" />
--- src/components/PropertiesManager/Text/PickerPopup/script.js
+++ src/components/PropertiesManager/Text/PickerPopup/script.js
@@ -1,1 +1,51 @@
-export default {};
+import { Photoshop } from 'vue-color';
+
+let defaultProps = {
+  hex: '#194d33e6',
+  hsl: {
+    h: 150,
+    s: 0.5,
+    l: 0.2,
+    a: 0.9
+  },
+  hsv: {
+    h: 150,
+    s: 0.66,
+    v: 0.3,
+    a: 0.9
+  },
+  rgba: {
+    r: 159,
+    g: 96,
+    b: 43,
+    a: 0.9
+  },
+  a: 0.9
+};
+
+export default {
+  components: {
+    'photoshop-picker': Photoshop
+  },
+  data() {
+    return {
+      colors: defaultProps
+    };
+  },
+  computed: {
+    bgc() {
+      return this.colors.hex;
+    }
+  },
+  methods: {
+    onOk() {
+      console.log('ok');
+    },
+    onCancel() {
+      console.log('cancel');
+    },
+    updateValue(value) {
+      this.colors = value;
+    }
+  }
+};
