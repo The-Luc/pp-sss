46e29e48d9db6a14bef0057a9432e6ffdd17effc
halik023 <liem.dang@successsoftware.global>
2021-05-26 13:39:35 +0700

    pp-123: add text box

--- src/views/CreateBook/PrintEdition/EditScreen/PageEdition/index.vue
+++ src/views/CreateBook/PrintEdition/EditScreen/PageEdition/index.vue
@@ -1,35 +1,6 @@
 <template>
   <div class="page-edition">
-    <div
-      class="container-edition"
-      :class="[
-        { 'canvas-soft-cover': isSoftCover },
-        { 'canvas-hard-cover': isHardCover },
-        { intro: isIntro },
-        { signature: isSignature }
-      ]"
-    >
-      <canvas id="page-left"></canvas>
-      <canvas id="page-right"></canvas>
-      <div class="page-container">
-        <div class="page">
-          <div class="page-left">
-            <div v-if="isIntro" class="front-cover">
-              <div>Inside Front Cover</div>
-              <div>THIS PAGE</div>
-              <div>DOES NOT PRINT</div>
-            </div>
-          </div>
-          <div class="page-right">
-            <div v-if="isSignature" class="front-cover">
-              <div>Inside Front Cover</div>
-              <div>THIS PAGE</div>
-              <div>DOES NOT PRINT</div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <canvas class="printCanvas" id="canvas" ref="canvas"></canvas>
   </div>
 </template>
 
