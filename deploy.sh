9a0cacdc4cf67a39fc6d88d68145ce1fdf59282f
sonarsuccesssoftware <84000057+sonarsuccesssoftware@users.noreply.github.com>
2021-05-30 14:14:10 +0700

    Add files via upload

---
+++ deploy.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+#
+# Shell script for remote deployment and execution of programs.
+#
+
+func_init(){
+	env=$ENV;
+}
+
+func_installEVN(){
+	nvm install 14.17.0
+}
+
+func_build(){
+	cd /home/ppdeploy/jenkins/workspace/dev-pp-tools
+	case "$env" in
+
+	1)  echo "dev"
+		npm run build-testing
+		;;
+	2)  echo  "qa"
+		npm run build-qa
+		;;
+	esac
+}
+
+func_deploy(){
+	case "$env" in
+
+	1)  echo "dev"
+		rm -rf /var/www/frontend/pp-tools-dev/*
+		cp -vRT /home/ppdeploy/jenkins/workspace/dev-pp-tools/dist/ /var/www/frontend/pp-tools-dev/
+		cd /var/www/frontend/pp-tools-dev
+		npm i
+		cp ../index.dev.js ./
+		pm2 start index.dev.js
+		;;
+	2)  echo  "qa"
+		rm -rf /var/www/frontend/pp-tools-dev/*
+		cp -vRT /home/ppdeploy/jenkins/workspace/dev-pp-tools/dist/ /var/www/frontend/pp-tools-qa/
+		cd /var/www/frontend/pp-tools-qa
+		npm i
+		cp ../index.dev.js ./
+		pm2 start index.dev.js
+		;;
+	esac
+}
+
+#
+# Main
+func_init;
+func_installEVN;
+func_build;
+func_deploy;
+
