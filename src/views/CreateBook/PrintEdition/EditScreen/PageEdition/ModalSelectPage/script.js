efbd3227c4dc7248cf9f0061425e22e3c18e8266
koo-sss <dong.dang@successsoftware.global>
2021-05-31 21:02:27 +0700

    PP-399: Fix override single layout

--- src/views/CreateBook/PrintEdition/EditScreen/PageEdition/ModalSelectPage/script.js
+++ src/views/CreateBook/PrintEdition/EditScreen/PageEdition/ModalSelectPage/script.js
@@ -1,10 +1,9 @@
-import { mapMutations } from 'vuex';
+import { mapMutations, mapGetters } from 'vuex';
 
 import Modal from '@/components/Modal';
 import { useDrawLayout } from '@/hooks';
-import { MUTATES } from '@/store/modules/book/const';
+import { GETTERS, MUTATES } from '@/store/modules/book/const';
 import { MUTATES as APP_MUTATES } from '@/store/modules/app/const';
-import { LAYOUT_TYPES } from '@/common/constants';
 
 export default {
   setup() {
@@ -17,14 +16,15 @@ export default {
     Modal
   },
   computed: {
+    ...mapGetters({
+      pageSelected: GETTERS.GET_PAGE_SELECTED,
+      sheetLayout: GETTERS.SHEET_LAYOUT
+    }),
     numberPageLeft() {
       return this.$attrs.props.numberPageLeft;
     },
     numberPageRight() {
       return this.$attrs.props.numberPageRight;
-    },
-    imageUrlLeft() {
-      return this.$attrs.props.imageUrlLeft;
     },
     sheetId() {
       return this.$attrs.props.sheetId;
@@ -41,29 +41,54 @@ export default {
       updateSheetThemeLayout: MUTATES.UPDATE_SHEET_THEME_LAYOUT,
       toggleModal: APP_MUTATES.TOGGLE_MODAL
     }),
+    /**
+     * Trigger mutation to close modal
+     */
     closeModal() {
       this.toggleModal({
         isOpenModal: false
       });
     },
+    /**
+     * Trigger mutation to update single layout for sheet
+     * @param  {String} pagePosition Left or right layout
+     */
     updateSheet(pagePosition) {
       this.updateSheetThemeLayout({
         sheetId: this.sheetId,
         themeId: this.themeId,
         layoutId: this.layoutId,
-        pagePosition,
-        layoutType: LAYOUT_TYPES.SINGLE_PAGE.value
+        pagePosition
       });
     },
-    onLeftClick() {
-      this.drawLayout(this.imageUrlLeft, '');
-      this.updateSheet('left');
+    /**
+     * Get sheet's layout and draw
+     */
+    drawLayoutSinglePage() {
+      const sheetLayout = this.sheetLayout(this.pageSelected);
+      const { imageUrlLeft, imageUrlRight } = sheetLayout;
+      this.drawLayout(imageUrlLeft, imageUrlRight);
+    },
+    /**
+     * Update layout to sheet, draw layout and then close modal
+     * @param  {String} pagePosition Left or right layout
+     */
+    onUpdateLayoutSheet(pagePosition) {
+      this.updateSheet(pagePosition);
+      this.drawLayoutSinglePage();
       this.closeModal();
     },
+    /**
+     * Draw left layout when user click button from modal
+     */
+    onLeftClick() {
+      this.onUpdateLayoutSheet('left');
+    },
+    /**
+     * Draw right layout when user click button from modal
+     */
     onRightClick() {
-      this.drawLayout('', this.imageUrlLeft);
-      this.updateSheet('right');
-      this.closeModal();
+      this.onUpdateLayoutSheet('right');
     }
   }
 };
