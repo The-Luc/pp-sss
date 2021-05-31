8c9688bce2b497e75b0f944b376a6f751a1ac0a0
Koo SSS <dong.dang@successsoftware.global>
2021-05-31 13:33:17 +0700

    PP-394: Just show properties when active object

--- src/components/PropertiesManager/Text/GeneralContent/Font/Family/script.js
+++ src/components/PropertiesManager/Text/GeneralContent/Font/Family/script.js
@@ -1,6 +1,8 @@
+import { mapGetters, mapMutations } from 'vuex';
+
 import PpSelect from '@/components/Select';
-import { mapGetters } from 'vuex';
 import { GETTERS } from '@/store/modules/book/const';
+import { MUTATES } from '@/store/modules/app/const';
 export default {
   components: {
     PpSelect
@@ -24,6 +26,10 @@ export default {
     })
   },
   methods: {
+    ...mapMutations({
+      setObjectTypeSelected: MUTATES.SET_OBJECT_TYPE_SELECTED,
+      toggleColorPicker: MUTATES.TOGGLE_COLOR_PICKER
+    }),
     /**
      * Change font family of text box selected
      * @param   {Object} data new font family of text box
@@ -59,6 +65,12 @@ export default {
      * Clear data of text properties modal
      */
     clearDataFontFamily() {
+      this.setObjectTypeSelected({
+        type: ''
+      });
+      this.toggleColorPicker({
+        isOpen: false
+      });
       setTimeout(() => {
         this.selectedVal = {
           value: 'arial'
