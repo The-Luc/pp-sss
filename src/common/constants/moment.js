6215ff827405abccd9c9b75143d3ecadf9d57797
vuln-successsoftware <vu.luunguyen@successsoftware.global>
2021-06-04 21:23:38 +0700

    PP-408: remove unused code

--- src/common/constants/moment.js
+++ src/common/constants/moment.js
@@ -10,16 +10,3 @@ export const MOMENT_TYPE = {
   SECOND: 'seconds',
   MILLISECOND: 'milliseconds'
 };
-
-export const MOMENT_SHORT_TYPE = {
-  YEAR: 'y',
-  QUATER: 'q',
-  MONTH: 'M',
-  WEEK: 'w',
-  DATE: 'D',
-  DAY: 'd',
-  HOUR: 'h',
-  MINUTE: 'm',
-  SECOND: 's',
-  MILLISECOND: 'ms'
-};
