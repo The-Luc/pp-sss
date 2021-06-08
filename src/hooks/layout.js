63bde4c2fa208517b6ecace33e03728715880af4
Giang Dao <giang.dao@successsoftware.global>
2021-06-08 22:54:23 +0700

    PP-438: adjust size

--- src/hooks/layout.js
+++ src/hooks/layout.js
@@ -11,8 +11,7 @@ import {
   GETTERS as APP_GETTERS
 } from '@/store/modules/app/const';
 import { OBJECT_TYPE, TOOL_NAME } from '@/common/constants';
-import { PRINT_CANVAS_SIZE } from '@/common/constants/canvas';
-import { computedObjectData } from '@/common/utils';
+import { scaleSize } from '@/common/utils';
 
 export const useLayoutPrompt = () => {
   const { isPrompt, pageSelected } = useGetters({
@@ -45,16 +44,9 @@ export const useLayoutPrompt = () => {
  * @param {Object} page - Page data
  * @param {String} position - Page position to draw left or right
  * @param {Ref} targetCanvas - Target canvas to draw objects
- * @param {Object} layoutSize - Layout size include width and height
  * @param {Array} objects - All objects user addeed
  */
-const handleDrawTextLayout = (
-  page,
-  position,
-  targetCanvas,
-  layoutSize,
-  objects
-) => {
+const handleDrawTextLayout = (page, position, targetCanvas, objects) => {
   const objectIds = cloneDeep(page.objects);
   const objectsData = pick(objects, [...objectIds]);
   let res = {};
@@ -65,9 +57,12 @@ const handleDrawTextLayout = (
       textObjects.push(res);
     }
   });
-  if (textObjects?.length > 0) {
-    textObjects.forEach(obj => {
-      const { rotation } = obj.coord;
+  Object.values(objectsData).forEach(obj => {
+    if (obj.type === OBJECT_TYPE.TEXT) {
+      const {
+        coord: { x, y, rotation },
+        size: { width, height }
+      } = obj;
       const {
         color,
         fontFamily,
@@ -79,29 +74,24 @@ const handleDrawTextLayout = (
         text
       } = obj.property;
 
-      const { left, top, width, height } = computedObjectData(
-        obj.coord,
-        obj.size,
-        targetCanvas,
-        layoutSize,
-        position
-      );
-
-      const fontSizeRatio =
-        (targetCanvas.width / PRINT_CANVAS_SIZE.WIDTH) * fontSize;
+      let left = scaleSize(x);
+      if (position === 'right') {
+        const baseLeft = targetCanvas.width / targetCanvas.getZoom() / 2;
+        left += baseLeft;
+      }
       const textObj = new fabric.Textbox(text, {
         lockUniScaling: false,
-        width,
-        height,
+        width: scaleSize(width),
+        height: scaleSize(height),
         left,
-        top,
+        top: scaleSize(y),
         fontWeight: isBold ? 'bold' : 'normal',
         fontStyle: isItalic ? 'italic' : 'normal',
         underline: isUnderline,
         textAlign: 'left',
         rotate: rotation,
         fontFamily,
-        fontSize: fontSizeRatio,
+        fontSize: scaleSize(fontSize),
         isBold,
         isItalic,
         isUnderline,
@@ -109,8 +99,8 @@ const handleDrawTextLayout = (
         fill: color
       });
       targetCanvas.add(textObj);
-    });
-  }
+    }
+  });
 };
 
 /**
@@ -146,7 +136,7 @@ const handleDrawBackgroundLayout = (
     img.scaleX = width / zoom / img.width / 2;
     img.scaleY = height / zoom / img.height;
     targetCanvas.add(img);
-    handleDrawTextLayout(pageData, position, targetCanvas, layoutSize, objects);
+    handleDrawTextLayout(pageData, position, targetCanvas, objects);
   });
 };
 
