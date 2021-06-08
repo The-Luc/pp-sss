c38b06a192cc2f132427ecfadb31eea9b716dbc5
liemdangsuccess <liem.dang@successsoftware.global>
2021-06-08 22:41:18 +0700

    pp-379: clip art properties genaral ui ux layout

--- src/containers/PropertiesManager/ClipArt/GeneralContent/Shadow/script.js
+++ src/containers/PropertiesManager/ClipArt/GeneralContent/Shadow/script.js
@@ -1,19 +1,26 @@
 import PpSelect from '@/components/Select';
+
 export default {
   components: {
     PpSelect
   },
-  data() {
-    return {
-      selectedVal: {
-        value: 'noShadow'
-      }
-    };
-  },
   props: {
-    items: {
+    selectedShadow: {
+      type: Object,
+      required: true
+    },
+    options: {
       type: Array,
       required: true
+    }
+  },
+  methods: {
+    /**
+     * Emit shadow value to parent
+     * @param   {Object}  value Value user selected
+     */
+    onChange(value) {
+      this.$emit('change', value);
     }
   }
 };
