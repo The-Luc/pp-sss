9b1ebe4f832a8455226bc1569e3a6f3cdbd9eb8a
koo-sss <dong.dang@successsoftware.global>
2021-06-08 21:35:07 +0700

    pp-225: update styled

--- src/components/Number/index.vue
+++ src/components/Number/index.vue
@@ -2,12 +2,14 @@
   <v-text-field
     :key="componentKey"
     class="pp-number"
+    :class="valueLength < 3 ? `length-${valueLength}` : 'length-3'"
     hide-details
     single-line
     :value="value"
     type="number"
     :suffix="suffix"
     @keydown.esc="onEsc"
+    @input="onNumberChange"
     @change="onChangeInput"
   />
 </template>
