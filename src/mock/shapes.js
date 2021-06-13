ca80520c5edcbcc3cf6936c04f82a218e690990e
vuln-successsoftware <vu.luunguyen@successsoftware.global>
2021-06-13 18:17:16 +0700

    PP-153: Can select / unselect shapes

---
+++ src/mock/shapes.js
@@ -0,0 +1,113 @@
+import { ShapeElement } from '@/common/models';
+
+import SHAPE_1 from '@/assets/image/shapes/shape-1.svg';
+import SHAPE_2 from '@/assets/image/shapes/shape-2.svg';
+import SHAPE_3 from '@/assets/image/shapes/shape-3.svg';
+import SHAPE_4 from '@/assets/image/shapes/shape-4.svg';
+import SHAPE_5 from '@/assets/image/shapes/shape-5.svg';
+import SHAPE_6 from '@/assets/image/shapes/shape-6.svg';
+import SHAPE_7 from '@/assets/image/shapes/shape-7.svg';
+import SHAPE_8 from '@/assets/image/shapes/shape-8.svg';
+import SHAPE_9 from '@/assets/image/shapes/shape-9.svg';
+import SHAPE_10 from '@/assets/image/shapes/shape-10.svg';
+import SHAPE_11 from '@/assets/image/shapes/shape-11.svg';
+import SHAPE_12 from '@/assets/image/shapes/shape-12.svg';
+
+export const SHAPES = [
+  {
+    ...ShapeElement,
+    id: 0,
+    property: {
+      thumbnail: SHAPE_1,
+      pathData: SHAPE_1
+    }
+  },
+  {
+    ...ShapeElement,
+    id: 1,
+    property: {
+      thumbnail: SHAPE_2,
+      pathData: SHAPE_2
+    }
+  },
+  {
+    ...ShapeElement,
+    id: 2,
+    property: {
+      thumbnail: SHAPE_3,
+      pathData: SHAPE_3
+    }
+  },
+  {
+    ...ShapeElement,
+    id: 3,
+    property: {
+      thumbnail: SHAPE_4,
+      pathData: SHAPE_4
+    }
+  },
+  {
+    ...ShapeElement,
+    id: 4,
+    property: {
+      thumbnail: SHAPE_5,
+      pathData: SHAPE_5
+    }
+  },
+  {
+    ...ShapeElement,
+    id: 5,
+    property: {
+      thumbnail: SHAPE_6,
+      pathData: SHAPE_6
+    }
+  },
+  {
+    ...ShapeElement,
+    id: 6,
+    property: {
+      thumbnail: SHAPE_7,
+      pathData: SHAPE_7
+    }
+  },
+  {
+    ...ShapeElement,
+    id: 7,
+    property: {
+      thumbnail: SHAPE_8,
+      pathData: SHAPE_8
+    }
+  },
+  {
+    ...ShapeElement,
+    id: 8,
+    property: {
+      thumbnail: SHAPE_9,
+      pathData: SHAPE_9
+    }
+  },
+  {
+    ...ShapeElement,
+    id: 9,
+    property: {
+      thumbnail: SHAPE_10,
+      pathData: SHAPE_10
+    }
+  },
+  {
+    ...ShapeElement,
+    id: 10,
+    property: {
+      thumbnail: SHAPE_11,
+      pathData: SHAPE_11
+    }
+  },
+  {
+    ...ShapeElement,
+    id: 11,
+    property: {
+      thumbnail: SHAPE_12,
+      pathData: SHAPE_12
+    }
+  }
+];
