2a5a3760efb9f3f81332c64580a4e47c5546f120
Giang Dao <giang.dao@successsoftware.global>
2021-06-17 16:34:35 +0700

    update object structure

--- src/common/models/elements.js
+++ src/common/models/elements.js
@@ -6,7 +6,18 @@ import {
   DEFAULT_BACKGROUND
 } from '@/common/constants';
 
-export const BaseProperty = {
+export const BaseElement = {
+  id: '',
+  type: '',
+  size: {
+    width: 0,
+    height: 0
+  },
+  coord: {
+    x: 0,
+    y: 0,
+    rotation: 0 // degree
+  },
   color: DEFAULT_PROP.COLOR,
   opacity: DEFAULT_PROP.OPACITY,
   border: {
@@ -26,30 +37,18 @@ export const BaseProperty = {
   flip: {
     horiziontal: false,
     vertical: false
-  }
+  },
+  zIndex: 0,
 };
 
 export const BasePrintProperty = {
-  ...BaseProperty
+  ...BaseElement
 };
 
 export const BaseDigitalProperty = {
-  ...BaseProperty
+  ...BaseElement
 };
 
-export const BaseElement = {
-  id: '',
-  type: '',
-  size: {
-    width: 0,
-    height: 0
-  },
-  coord: {
-    x: 0,
-    y: 0,
-    rotation: 0 // degree
-  }
-};
 
 export const TextElement = {
   ...BaseElement,
@@ -59,91 +58,76 @@ export const TextElement = {
     y: DEFAULT_TEXT.COORD.Y,
     rotation: DEFAULT_TEXT.COORD.ROTATION // degree
   },
-  property: {
-    ...BasePrintProperty,
-    styleId: DEFAULT_TEXT.STYLE_ID,
-    text: DEFAULT_TEXT.TEXT,
-    fontFamily: DEFAULT_TEXT.FONT_FAMILY,
-    fontSize: DEFAULT_TEXT.FONT_SIZE,
-    isBold: DEFAULT_TEXT.IS_BOLD,
-    isItalic: DEFAULT_TEXT.IS_ITALIC,
-    isUnderline: DEFAULT_TEXT.IS_UNDERLINE,
-    textCase: DEFAULT_TEXT.TEXT_CASE, // UPPERCASE, LOWERCASE, CAPITALIZE
-    alignment: {
-      horiziontal: DEFAULT_TEXT.ALIGNMENT.HORIZIONTAL, // LEFT, CENTER, RIGHT, JUSTIFY
-      vertical: DEFAULT_TEXT.ALIGNMENT.VERTICAL // TOP, MIDDLE, BOTTOM
-    },
-    letterSpacing: DEFAULT_TEXT.LETTER_SPACING,
-    lineSpacing: DEFAULT_TEXT.LINE_SPACING, // 1.2 * em
-    lineHeight: DEFAULT_TEXT.LINE_HEIGHT,
-    column: DEFAULT_TEXT.COLUMN,
-    opacity: DEFAULT_TEXT.OPACITY,
-    editingBorderColor: DEFAULT_TEXT.EDITING_BORDER_COLOR,
-    border: {
-      fill: DEFAULT_TEXT.BORDER.FILL,
-      stroke: DEFAULT_TEXT.BORDER.STROKE,
-      strokeWidth: DEFAULT_TEXT.BORDER.STROKE_WIDTH,
-      strokeDashArray: DEFAULT_TEXT.BORDER.STROKE_DASH_ARRAY,
-      strokeLineCap: DEFAULT_TEXT.BORDER.STROKE_LINE_CAP
-    },
-    shadow: {
-      dropShadow: DEFAULT_TEXT.SHADOW.DROP_SHADOW,
-      shadowBlur: DEFAULT_TEXT.SHADOW.BLUR,
-      shadowOffset: DEFAULT_TEXT.SHADOW.OFFSET,
-      shadowOpacity: DEFAULT_TEXT.SHADOW.OPACITY,
-      shadowAngle: DEFAULT_TEXT.SHADOW.ANGLE,
-      shadowColor: DEFAULT_TEXT.SHADOW.COLOR
-    }
+  styleId: DEFAULT_TEXT.STYLE_ID,
+  text: DEFAULT_TEXT.TEXT,
+  fontFamily: DEFAULT_TEXT.FONT_FAMILY,
+  fontSize: DEFAULT_TEXT.FONT_SIZE,
+  isBold: DEFAULT_TEXT.IS_BOLD,
+  isItalic: DEFAULT_TEXT.IS_ITALIC,
+  isUnderline: DEFAULT_TEXT.IS_UNDERLINE,
+  textCase: DEFAULT_TEXT.TEXT_CASE, // UPPERCASE, LOWERCASE, CAPITALIZE
+  alignment: {
+    horiziontal: DEFAULT_TEXT.ALIGNMENT.HORIZIONTAL, // LEFT, CENTER, RIGHT, JUSTIFY
+    vertical: DEFAULT_TEXT.ALIGNMENT.VERTICAL // TOP, MIDDLE, BOTTOM
+  },
+  letterSpacing: DEFAULT_TEXT.LETTER_SPACING,
+  lineSpacing: DEFAULT_TEXT.LINE_SPACING, // 1.2 * em
+  lineHeight: DEFAULT_TEXT.LINE_HEIGHT,
+  column: DEFAULT_TEXT.COLUMN,
+  opacity: DEFAULT_TEXT.OPACITY,
+  editingBorderColor: DEFAULT_TEXT.EDITING_BORDER_COLOR,
+  border: {
+    fill: DEFAULT_TEXT.BORDER.FILL,
+    stroke: DEFAULT_TEXT.BORDER.STROKE,
+    strokeWidth: DEFAULT_TEXT.BORDER.STROKE_WIDTH,
+    strokeDashArray: DEFAULT_TEXT.BORDER.STROKE_DASH_ARRAY,
+    strokeLineCap: DEFAULT_TEXT.BORDER.STROKE_LINE_CAP
+  },
+  shadow: {
+    dropShadow: DEFAULT_TEXT.SHADOW.DROP_SHADOW,
+    shadowBlur: DEFAULT_TEXT.SHADOW.BLUR,
+    shadowOffset: DEFAULT_TEXT.SHADOW.OFFSET,
+    shadowOpacity: DEFAULT_TEXT.SHADOW.OPACITY,
+    shadowAngle: DEFAULT_TEXT.SHADOW.ANGLE,
+    shadowColor: DEFAULT_TEXT.SHADOW.COLOR
   }
 };
 
 export const ImageElement = {
   ...BaseElement,
   type: OBJECT_TYPE.IMAGE,
-  property: {
-    ...BasePrintProperty,
-    categoryId: 'Cover',
-    name: '',
-    thumbnail: DEFAULT_IMAGE.thumbnail,
-    imageUrl: DEFAULT_IMAGE.imageUrl
-  }
+  categoryId: 'Cover',
+  name: '',
+  thumbnail: DEFAULT_IMAGE.thumbnail,
+  imageUrl: DEFAULT_IMAGE.imageUrl
 };
 
 export const BackgroundElement = {
   ...BaseElement,
   type: OBJECT_TYPE.BACKGROUND,
-  property: {
-    ...BasePrintProperty,
-    categoryId: '',
-    type: DEFAULT_BACKGROUND.TYPE,
-    pageType: DEFAULT_BACKGROUND.PAGE_TYPE,
-    name: '',
-    thumbnail: '',
-    imageUrl: ''
-  }
+  categoryId: '',
+  type: DEFAULT_BACKGROUND.TYPE,
+  pageType: DEFAULT_BACKGROUND.PAGE_TYPE,
+  name: '',
+  thumbnail: '',
+  imageUrl: ''
 };
 
 export const ClipArtElement = {
   ...BaseElement,
   type: OBJECT_TYPE.CLIP_ART,
-  property: {
-    ...BasePrintProperty,
-    category: '',
-    name: '',
-    thumbnail: '',
-    vector: '' // imgUrl
-  }
+  category: '',
+  name: '',
+  thumbnail: '',
+  vector: '' // imgUrl
 };
 
 export const ShapeElement = {
   ...BaseElement,
   type: OBJECT_TYPE.SHAPE,
-  property: {
-    ...BasePrintProperty,
-    name: '',
-    thumbnail: '',
-    pathData: 'img.svg', // TODO: Need discuss with FM to get instruction on using shape
-    color: DEFAULT_PROP.COLOR,
-    stroke: DEFAULT_PROP.COLOR
-  }
+  name: '',
+  thumbnail: '',
+  pathData: 'img.svg', // TODO: Need discuss with FM to get instruction on using shape
+  color: DEFAULT_PROP.COLOR,
+  stroke: DEFAULT_PROP.COLOR
 };
