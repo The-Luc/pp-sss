e267004a1c1e5caa9228cd91a5514385d8248310
Koo-SSS <83195925+Koo-SSS@users.noreply.github.com>
2021-06-22 13:19:18 +0700

    Feature/pp 195 copy item (#224)
    * pp-195: Create actions tool UI
    
    * pp-195: Create constant for actions value

---
+++ src/containers/ToolPopoverManager/Actions/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="actions-container">
+    <Item v-for="item in items" :key="item.value" :item="item" />
+  </div>
+</template>
+
+<script src="./script.js" />
+<style lang="scss" src="./style.scss" scoped />
