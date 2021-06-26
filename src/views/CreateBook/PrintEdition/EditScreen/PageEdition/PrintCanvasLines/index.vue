441aa3b866f0ca29a67fa75c80c1660f5b2d4b93
Giang Dao <giangdao@success-ss.com.vn>
2021-06-26 13:45:24 +0700

    PP-532: fix ruler alignment (#258)
    Co-authored-by: Giang Dao <giang.dao@successsoftware.global>

--- src/views/CreateBook/PrintEdition/EditScreen/PageEdition/PrintCanvasLines/index.vue
+++ src/views/CreateBook/PrintEdition/EditScreen/PageEdition/PrintCanvasLines/index.vue
@@ -1,23 +1,34 @@
 <template>
   <div class="page-indicators" :style="containerStyle">
-    <div
-      v-if="!isHalfSheet"
-      class="print-spines"
-      :class="spineConfig.single ? 'single' : 'double'"
-      :style="spineConfig.style"
-    >
-      &nbsp;
-    </div>
-    <div v-if="isFrontCover" class="print-inside-front-cover">
-      <div class="print-message-box">
-        <p class="print-title">Inside Front Cover</p>
-        <p class="print-subtitle">THIS PAGE<br />DOES NOT PRINT</p>
+    <div class="print-inside-cover">
+      <div
+        class="print-inside-front-cover"
+        :style="{
+          visibility: isFrontCover ? 'visible' : 'hidden'
+        }"
+      >
+        <div class="print-message-box">
+          <p class="print-title">Inside Front Cover</p>
+          <p class="print-subtitle">THIS PAGE<br />DOES NOT PRINT</p>
+        </div>
       </div>
-    </div>
-    <div v-if="isBackCover" class="print-inside-back-cover">
-      <div class="print-message-box">
-        <p class="print-title">Inside Back Cover</p>
-        <p class="print-subtitle">THIS PAGE<br />DOES NOT PRINT</p>
+      <div
+        class="print-spines"
+        :class="spineConfig.single ? 'single' : 'double'"
+        :style="spineConfig.style"
+      >
+        &nbsp;
+      </div>
+      <div
+        class="print-inside-back-cover"
+        :style="{
+          visibility: isBackCover ? 'visible' : 'hidden'
+        }"
+      >
+        <div class="print-message-box">
+          <p class="print-title">Inside Back Cover</p>
+          <p class="print-subtitle">THIS PAGE<br />DOES NOT PRINT</p>
+        </div>
       </div>
     </div>
     <div class="print-bleed" :style="bleedStyle">
