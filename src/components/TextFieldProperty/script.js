4a0a57173bfcd95eccc2dc8e9b1bfd1390b4c776
LucThi-ss <85859772+LucThi-ss@users.noreply.github.com>
2021-06-27 22:27:20 +0700

    PT-432: handle input ESC event (#263)
    * PT-432: handle input ESC event
    
    * PP-432: use flag to handle ESC event
    
    Co-authored-by: LucThi-ss <luc.thi@succeesssoftware.global>

--- src/components/TextFieldProperty/script.js
+++ src/components/TextFieldProperty/script.js
@@ -17,19 +17,33 @@ export default {
       default: false
     }
   },
+  data() {
+    return {
+      // when ESC is pressed, both onEsc and onChange are called
+      // we need the following variable to prevent onChange triggered when ESC pressed
+      isAllowToChange: true
+    };
+  },
   methods: {
     /**
      * Emit event change input
      * @param   {Object}   event event change change of input
      */
     onChange(event) {
+      if (!this.isAllowToChange) return;
+
       this.$emit('change', event.target.value);
     },
     /**
      * Emit event user press Escape
      */
     onEsc() {
+      this.isAllowToChange = false;
+
       this.$emit('change', this.value);
     }
+  },
+  updated() {
+    this.isAllowToChange = true;
   }
 };
