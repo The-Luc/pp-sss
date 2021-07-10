80a428d353f39a54dd324b00d1f98b485ebda2a2
LucThi-ss <luc.thi@succeesssoftware.global>
2021-07-10 11:24:58 +0700

    PP-147: Revised digital layout mock data

--- src/mock/digitalLayouts.js
+++ src/mock/digitalLayouts.js
@@ -33,24 +33,9 @@ import LAYOUT_THUMB_21 from '@/assets/image/digital-layouts/thumb/layout-21.jpg'
 import LAYOUT_01 from '@/assets/image/digital-layouts/layout-1.jpg';
 import LAYOUT_02 from '@/assets/image/digital-layouts/layout-2.jpg';
 import LAYOUT_03 from '@/assets/image/digital-layouts/layout-3.jpg';
-// import LAYOUT_04 from '@/assets/image/digital-layouts/layout-4.jpg';
-// import LAYOUT_05 from '@/assets/image/digital-layouts/layout-5.jpg';
-// import LAYOUT_06 from '@/assets/image/digital-layouts/layout-6.jpg';
-// import LAYOUT_07 from '@/assets/image/digital-layouts/layout-7.jpg';
-// import LAYOUT_08 from '@/assets/image/digital-layouts/layout-8.jpg';
-// import LAYOUT_09 from '@/assets/image/digital-layouts/layout-9.jpg';
-// import LAYOUT_10 from '@/assets/image/digital-layouts/layout-10.jpg';
-// import LAYOUT_11 from '@/assets/image/digital-layouts/layout-11.jpg';
-// import LAYOUT_12 from '@/assets/image/digital-layouts/layout-12.jpg';
-// import LAYOUT_13 from '@/assets/image/digital-layouts/layout-13.jpg';
-// import LAYOUT_14 from '@/assets/image/digital-layouts/layout-14.jpg';
-// import LAYOUT_15 from '@/assets/image/digital-layouts/layout-15.jpg';
-// import LAYOUT_16 from '@/assets/image/digital-layouts/layout-16.jpg';
-// import LAYOUT_17 from '@/assets/image/digital-layouts/layout-17.jpg';
-// import LAYOUT_18 from '@/assets/image/digital-layouts/layout-18.jpg';
-// import LAYOUT_19 from '@/assets/image/digital-layouts/layout-19.jpg';
-// import LAYOUT_20 from '@/assets/image/digital-layouts/layout-20.jpg';
-// import LAYOUT_21 from '@/assets/image/digital-layouts/layout-21.jpg';
+import LAYOUT_04 from '@/assets/image/digital-layouts/layout-4.jpg';
+import LAYOUT_05 from '@/assets/image/digital-layouts/layout-5.jpg';
+import LAYOUT_06 from '@/assets/image/digital-layouts/layout-6.jpg';
 
 import LPCA_04531 from '@/assets/image/layouts/background/LPCA_04531.png';
 import LPCA_04511 from '@/assets/image/layouts/background/LPCA_04511.png';
@@ -99,38 +84,45 @@ const text2 = {
   color: '#FF0000'
 };
 
-const bg1 = {
-  ...BackgroundElement,
-  id: uniqueId(),
-  pageType: BACKGROUND_PAGE_TYPE.FULL_PAGE.id,
-  backgroundType: '',
-  category: 'Cover',
-  name: 'watercolorbackground.jpg',
-  thumbnail: LAYOUT_THUMB_01,
-  imageUrl: LAYOUT_01
-};
+const backgroundData = [
+  {
+    thumbnail: LAYOUT_THUMB_01,
+    imageUrl: LAYOUT_01
+  },
+  {
+    thumbnail: LAYOUT_THUMB_02,
+    imageUrl: LAYOUT_02
+  },
+  {
+    thumbnail: LAYOUT_THUMB_03,
+    imageUrl: LAYOUT_03
+  },
+  {
+    thumbnail: LAYOUT_THUMB_04,
+    imageUrl: LAYOUT_04
+  },
+  {
+    thumbnail: LAYOUT_THUMB_05,
+    imageUrl: LAYOUT_05
+  },
+  {
+    thumbnail: LAYOUT_THUMB_06,
+    imageUrl: LAYOUT_06
+  }
+];
 
-const bg2 = {
-  ...BackgroundElement,
-  id: uniqueId(),
-  pageType: BACKGROUND_PAGE_TYPE.FULL_PAGE.id,
-  backgroundType: '',
-  category: 'Cover',
-  name: 'watercolorbackground.jpg',
-  thumbnail: LAYOUT_THUMB_02,
-  imageUrl: LAYOUT_02
-};
-
-const bg3 = {
-  ...BackgroundElement,
-  id: uniqueId(),
-  pageType: BACKGROUND_PAGE_TYPE.FULL_PAGE.id,
-  backgroundType: '',
-  category: 'Cover',
-  name: 'fullbackground.jpg',
-  thumbnail: LAYOUT_THUMB_03,
-  imageUrl: LAYOUT_03
-};
+const backgrounds = backgroundData.map((data, idx) => {
+  return {
+    ...BackgroundElement,
+    id: uniqueId(),
+    pageType: BACKGROUND_PAGE_TYPE.FULL_PAGE.id,
+    backgroundType: '',
+    category: 'Cover',
+    name: 'watercolorbackground.jpg',
+    thumbnail: backgroundData[idx].thumbnail,
+    imageUrl: backgroundData[idx].imageUrl
+  };
+});
 
 const clipArt1 = {
   ...ClipArtElement,
@@ -170,36 +162,36 @@ const clipArt2 = {
   vector: LPCA_04511
 };
 
-// const shape1 = {
-//   ...ShapeElement,
-//   id: uniqueId(),
-//   size: {
-//     width: 3,
-//     height: 3
-//   },
-//   coord: {
-//     x: 5,
-//     y: 5,
-//     rotation: 0 // degree
-//   },
-//   category: '',
-//   name: '',
-//   thumbnail: '',
-//   pathData: 'img.svg' // TODO: Need discuss with FM to get instruction on using shape
-// };
+const shape1 = {
+  ...ShapeElement,
+  id: uniqueId(),
+  size: {
+    width: 3,
+    height: 3
+  },
+  coord: {
+    x: 5,
+    y: 5,
+    rotation: 0 // degree
+  },
+  category: '',
+  name: '',
+  thumbnail: '',
+  pathData: 'img.svg' // TODO: Need discuss with FM to get instruction on using shape
+};
 
 const frames = [
   {
     id: 1,
     fromLayout: true,
-    objects: [bg1, text1, text2],
+    objects: [backgrounds[0], text1, text2],
     previewImageUrl: LAYOUT_THUMB_01,
     isVisited: false
   },
   {
     id: 2,
     fromLayout: true,
-    objects: [bg2, clipArt1, clipArt2],
+    objects: [backgrounds[1], clipArt1, clipArt2],
     previewImageUrl: LAYOUT_THUMB_02,
     isVisited: false
   }
@@ -209,21 +201,21 @@ const frames2 = [
   {
     id: 1,
     fromLayout: true,
-    objects: [bg3, text1, text2],
+    objects: [backgrounds[3], text1, text2],
     previewImageUrl: LAYOUT_THUMB_03,
     isVisited: false
   },
   {
     id: 2,
     fromLayout: true,
-    objects: [bg2, clipArt1, clipArt2],
+    objects: [backgrounds[4], clipArt1, shape1],
     previewImageUrl: LAYOUT_THUMB_04,
     isVisited: false
   },
   {
     id: 3,
     fromLayout: true,
-    objects: [bg1, clipArt1, clipArt2],
+    objects: [backgrounds[4], clipArt1, clipArt2],
     previewImageUrl: LAYOUT_THUMB_05,
     isVisited: false
   }
@@ -232,14 +224,14 @@ const supplementalFrames = [
   {
     id: 11,
     fromLayout: false,
-    objects: [bg1, text1, text2],
+    objects: [backgrounds[5], text1, text2],
     previewImageUrl: LAYOUT_THUMB_01,
     isVisited: false
   },
   {
     id: 12,
     fromLayout: false,
-    objects: [bg3, clipArt1, clipArt2],
+    objects: [backgrounds[6], clipArt1, clipArt2],
     previewImageUrl: LAYOUT_THUMB_02,
     isVisited: false
   }
@@ -275,7 +267,7 @@ export const packageLayouts = [
   },
   {
     id: 4,
-    name: 'Three Frame + A Supp',
+    name: 'Three Frames + A Supp',
     type: LAYOUT_TYPES.COLLAGE.value,
     frames: cloneDeep([...frames2, supplementalFrames[0]]),
     isFavorites: false,
@@ -284,12 +276,12 @@ export const packageLayouts = [
   },
   {
     id: 5,
-    name: 'Tokyo',
-    type: LAYOUT_TYPES.GRADUATION.value,
-    frames: cloneDeep([...frames]),
+    name: 'Two Frames + Two Supp',
+    type: LAYOUT_TYPES.COLLAGE.value,
+    frames: cloneDeep([...frames, ...supplementalFrames]),
     isFavorites: false,
     previewImageUrl: LAYOUT_THUMB_05,
-    themeId: 4
+    themeId: 1
   },
   {
     id: 6,
