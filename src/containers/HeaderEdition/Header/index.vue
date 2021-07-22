4536133bcf56f9ff0f8f230e18890a1b83bfe499
LucThi-ss <luc.thi@succeesssoftware.global>
2021-07-22 16:34:33 +0700

    PP-133: Implement UI for auto save

--- src/containers/HeaderEdition/Header/index.vue
+++ src/containers/HeaderEdition/Header/index.vue
@@ -4,6 +4,11 @@
       <PpButton type="btn" class="btn-save" @click="onClickSave">
         SAVE & EXIT
       </PpButton>
+      <div class="auto-save__container">
+        <div class="auto-save__text">Autosaving...</div>
+        <!-- <div class="auto-save-text">Saving</div> -->
+        <div class="auto-save__bar"></div>
+      </div>
       <span>{{ nameEditor }}</span>
     </div>
   </div>
