eba92e22916fcc0a2d4b8f786804af2028b41b34
liemdangsuccess <83195643+liemdangsuccess@users.noreply.github.com>
2021-08-03 22:49:52 +0700

    pp-172: create mock data photos (#502)
    * pp-172: create mock data
    
    * pp-172: fix comment
    
    * pp-172: fix comment
    
    * pp-172: fix comment

---
+++ src/common/constants/assetType.js
@@ -0,0 +1,3 @@
+export const ASSET_TYPE = {
+  PICTURE: 'Picture'
+};
---
+++ src/common/models/entities/asset.js
@@ -0,0 +1,30 @@
+import { BaseElementEntity } from './elements';
+import { ASSET_TYPE } from '@/common/constants';
+export class AssetEntity extends BaseElementEntity {
+  type = '';
+  mediaFileName = '';
+
+  /**
+   * @param {AssetEntity} props
+   */
+  constructor(props) {
+    super(props);
+    this._set(props);
+  }
+}
+
+export class PictureAssetEntity extends AssetEntity {
+  type = ASSET_TYPE.PICTURE;
+  originalHeight = 0;
+  originalWidth = 0;
+  thumbUrl = '';
+  imageUrl = '';
+
+  /**
+   * @param {PictureAssetEntity} props
+   */
+  constructor(props) {
+    super(props);
+    this._set(props);
+  }
+}
