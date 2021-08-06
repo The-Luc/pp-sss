112a369940ab5d68f866ab7a2987a0b48e118b5d
nhanhuynhsuccess <nhanhuynh@successsoftware.global>
2021-08-06 19:55:09 +0700

    pp-171:  JSDOC for method

--- src/components/Tags/script.js
+++ src/components/Tags/script.js
@@ -7,6 +7,11 @@ export default {
     }
   },
   methods: {
+    /**
+     * Trigger emit event when click got it
+     * @param  {Object} keyword value and status (active/inactive) of keyword
+     * @param  {Boolean} isNative check status (active/inactive) of keyword
+     */
     onclick(keyword, isNative) {
       if (isNative && keyword.active) return;
       this.$emit('click', keyword);
