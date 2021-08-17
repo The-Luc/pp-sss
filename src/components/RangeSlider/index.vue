87777e5978512e84b90b33ab1253a719174f558a
LucThi-ss <luc.thi@succeesssoftware.global>
2021-08-17 10:12:50 +0700

    PP-825: UI bugs

--- src/components/RangeSlider/index.vue
+++ src/components/RangeSlider/index.vue
@@ -1,24 +1,22 @@
 <template>
   <div class="pp-range-slider-container">
-    <div class="control-input">
-      <v-range-slider
-        ref="rangeSlider"
-        class="pp-range-slider"
-        :max="max"
-        :min="min"
-        :value="value"
-        :track-color="trackColor"
-        :track-fill-color="trackFillColor"
-        :thumb-color="thumbColor"
-        height="3"
-        loader-height="3"
-        hide-details
-        width="100%"
-        :disabled="disabled"
-        @change="onSliderChange"
-      >
-      </v-range-slider>
-    </div>
+    <v-range-slider
+      ref="rangeSlider"
+      class="pp-range-slider"
+      :max="max"
+      :min="min"
+      :value="value"
+      :track-color="trackColor"
+      :track-fill-color="trackFillColor"
+      :thumb-color="thumbColor"
+      height="3"
+      loader-height="3"
+      hide-details
+      width="100%"
+      :disabled="disabled"
+      @change="onSliderChange"
+    >
+    </v-range-slider>
   </div>
 </template>
 
