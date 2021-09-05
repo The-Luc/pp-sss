7cc071aa7d6308f305d7bfc43b4e616eaa0f80a0
LucThi-ss <luc.thi@succeesssoftware.global>
2021-09-06 06:36:59 +0700

    PP-830: Teacher setting UI

--- src/containers/Modals/PortraitFlow/FlowSettings/SettingContent/PortraitSetting/index.vue
+++ src/containers/Modals/PortraitFlow/FlowSettings/SettingContent/PortraitSetting/index.vue
@@ -2,9 +2,15 @@
   <div class="portrait-setting-container">
     <layout-setting
       :layout-settings="flowSettings.layoutSettings"
+      :is-single-folder="isSingleFolder"
       @change="onLayoutChange"
     />
-    <teacher-setting />
+    <teacher-setting
+      :teacher-settings="flowSettings.teacherSettings"
+      :layout="layout"
+      :is-single-folder="isSingleFolder"
+      @change="onTeacherSettingsChange"
+    />
   </div>
 </template>
 
