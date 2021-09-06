604bef6ee9e3dbd436951f1c5ec741fe39f35c96
LucThi-ss <luc.thi@succeesssoftware.global>
2021-09-06 20:27:08 +0700

    PP-293: Layout settings input validation

--- src/components/Portrait/PreviewThumbnail/script.js
+++ src/components/Portrait/PreviewThumbnail/script.js
@@ -67,6 +67,12 @@ export default {
       portraitsEl.style.setProperty('--col-count', this.layout.colCount);
 
       portraitsEl.style.setProperty('--portrait-width', portraitWidth + 'px');
+
+      let align = 'space-between';
+      if (row === 1 && col === 1) {
+        align = 'center';
+      }
+      portraitsEl.style.setProperty('--align', align);
     },
 
     /**
