00197c71237155353296a59a532a506736b49420
LucThi-ss <85859772+LucThi-ss@users.noreply.github.com>
2021-09-09 12:29:27 +0700

    Resolve QA conflicts (#732)
    Co-authored-by: LucThi-ss <luc.thi@succeesssoftware.global>

---
+++ src/containers/Modals/PortraitFlow/SettingContent/TextSetting/TextMargin/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="margin">
+    <div class="text-margin">Text Margins:</div>
+    <div class="box-margin">
+      Top Margin
+      <PpCombobox
+        :items="textMargin"
+        :margin-menu="0"
+        :nudge-width="80"
+        :selected-val="topMargin"
+        :appended-icon="appendedIcon"
+        @change="onChangeTopMargin"
+      />
+    </div>
+    <div class="box-margin">
+      Left Margin
+      <PpCombobox
+        :items="textMargin"
+        :margin-menu="0"
+        :nudge-width="80"
+        :selected-val="leftMargin"
+        :appended-icon="appendedIcon"
+        @change="onChangeLeftMargin"
+      />
+    </div>
+    <div class="box-margin">
+      Right Margin
+      <PpCombobox
+        :items="textMargin"
+        :margin-menu="0"
+        :nudge-width="80"
+        :selected-val="rightMargin"
+        :appended-icon="appendedIcon"
+        @change="onChangeRightMargin"
+      />
+    </div>
+    <div class="box-margin">
+      Bottom Margin
+      <PpCombobox
+        :items="textMargin"
+        :margin-menu="0"
+        :nudge-width="80"
+        :selected-val="bottomMargin"
+        :appended-icon="appendedIcon"
+        @change="onChangeBottomMargin"
+      />
+    </div>
+  </div>
+</template>
+
+<script src="./script.js"></script>
+
+<style lang="scss" src="./style.scss" scoped />
---
+++ src/containers/Modals/PortraitFlow/SettingContent/TextSetting/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="setting-container">
+    <div class="title-setting">
+      Text Settings
+    </div>
+    <PageTitle @change="onChange" />
+    <Properties @change="onChange" />
+    <TextMargin @change="onChange" />
+    <div class="name-text">Name Text:</div>
+    <Properties @change="onChange" />
+    <DisplayPosition @change="onChange" />
+  </div>
+</template>
+
+<script src="./script.js"></script>
+
+<style lang="scss" src="./style.scss" scoped />
