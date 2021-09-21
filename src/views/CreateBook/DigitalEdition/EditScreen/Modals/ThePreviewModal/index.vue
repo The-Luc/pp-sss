34a1dc7c0fd775e75f4a37022739a66209f5c9fc
vuln-successsoftware <83195652+vuln-successsoftware@users.noreply.github.com>
2021-09-22 00:05:51 +0700

    PP-962: Transition Wipe (#824)

--- src/views/CreateBook/DigitalEdition/EditScreen/Modals/ThePreviewModal/index.vue
+++ src/views/CreateBook/DigitalEdition/EditScreen/Modals/ThePreviewModal/index.vue
@@ -11,7 +11,33 @@
       @cancel="onClose"
     >
       <div class="container">
-        <transition :name="transitionType">
+        <transition-group
+          v-if="transition === 5"
+          :name="transitionType"
+          tag="div"
+          class="wrapper"
+        >
+          <div
+            :key="`img-%${imageKey}`"
+            class="thumb"
+            :style="{
+              backgroundImage: `url(${imageUrl})`,
+              backgroundPosition,
+              transition: transitionCss
+            }"
+          ></div>
+
+          <div
+            :key="`mask-%${imageKey}`"
+            class="thumb-mask"
+            :style="{
+              backgroundImage: `url(${maskUrl})`,
+              transition: transitionCss
+            }"
+          ></div>
+        </transition-group>
+
+        <transition v-else :name="transitionType">
           <div
             :key="imageKey"
             class="thumb"
