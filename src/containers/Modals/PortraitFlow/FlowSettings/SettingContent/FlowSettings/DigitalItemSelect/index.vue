39db16a6d1e05f9a50492cae5a3202fa7be4c35d
vuln-successsoftware <83195652+vuln-successsoftware@users.noreply.github.com>
2021-10-07 11:21:32 +0700

    QA (#916)
    * pp-1029: fix sonar
    
    * pp-1029: create component digitalItemSelect
    
    * pp-1029: fix code
    
    * pp-1029: set name new section
    
    * Fix mock data (#915)
    
    Co-authored-by: liemdangsuccess <liem.dang@successsoftware.global>
    Co-authored-by: An Nguyen <86215270+annguyensuccess@users.noreply.github.com>

---
+++ src/containers/Modals/PortraitFlow/FlowSettings/SettingContent/FlowSettings/DigitalItemSelect/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="digital-item-select-container">
+    <div class="select-container">
+      <item-select
+        v-if="indexItem !== 0"
+        :title="`Folder ${indexItem + 1}:`"
+        descript="Portrait flow starts on frame:"
+        :selected-val="item.selectedVal"
+        :items="item.frameOptions"
+        @change="onFrameSettingChange"
+      ></item-select>
+      <item-select
+        v-if="indexItem !== 0 && !isSingleScreen"
+        class="screen-select"
+        :title="''"
+        descript="of"
+        :selected-val="item.selectedValScreen"
+        :items="item.screenOptions"
+        @change="onScreenSettingChange"
+      ></item-select>
+    </div>
+    <div class="select-container">
+      <item-select
+        v-if="indexItem !== lengthDataSelect - 1"
+        :title="indexItem === 0 ? `Folder ${indexItem + 1}:` : ''"
+        descript="Portrait flow ends on frame:"
+        :disabled="true"
+        :selected-val="item.selectedValEndOnFrame"
+        :items="item.endOnFrameOptions"
+      ></item-select>
+      <item-select
+        v-if="indexItem !== lengthDataSelect - 1 && !isSingleScreen"
+        class="screen-select"
+        :title="''"
+        descript="of"
+        :disabled="true"
+        :selected-val="item.selectedValScreen"
+        :items="item.screenOptions"
+      ></item-select>
+    </div>
+  </div>
+</template>
+
+<script src="./script.js"></script>
+
+<style lang="scss" src="./style.scss" scoped />
