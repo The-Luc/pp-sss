10b2a1c7091dc3184c30b6ab878e86d7d5e5e970
vuln-successsoftware <vu.luunguyen@successsoftware.global>
2021-11-11 00:23:03 +0700

    PP-XX: Get layout favorites

---
+++ src/api/user/api_mutation.js
@@ -0,0 +1,32 @@
+import { graphqlRequest } from '../urql';
+
+import { loginUserMutation, saveFavoritesMutation } from './mutations';
+
+import { STATUS } from '@/common/constants';
+
+export const logInUser = async (email, password) => {
+  const res = await graphqlRequest(loginUserMutation, {
+    email,
+    password
+  });
+
+  if (res.status === STATUS.NG) return [];
+
+  const user = res.data.login_user;
+  const communityUserId = user.communities_users[0]?.id;
+
+  return {
+    token: user.token,
+    communityUserId
+  };
+};
+
+/**
+ * Save layout id to favorites
+ *
+ * @param   {Number | String} id  id of selected layout
+ * @returns {Object}              mutation result
+ */
+export const saveToFavorites = async id => {
+  return graphqlRequest(saveFavoritesMutation, { id });
+};
