b5fdee511d92721666391e704feebe0072451d21
vuln-successsoftware <vu.luunguyen@successsoftware.global>
2021-11-13 17:40:10 +0700

    Refactor Portrait api

--- src/hooks/portrait.js
+++ src/hooks/portrait.js
@@ -1,9 +1,9 @@
-import { getPortraitFolders } from '@/api/portrait';
+import { getPortraitFoldersApi } from '@/api/portrait';
 import { saveSelectedPortraitFolders } from '@/api/portraitService';
 
 export const usePortrait = () => {
   return {
-    getPortraitFolders,
+    getPortraitFolders: getPortraitFoldersApi,
     saveSelectedPortraitFolders
   };
 };
