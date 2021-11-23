a82ea0fce18310a4e70fc6441630278eb829bb6c
The-Luc Thi <luc.thi@successsoftware.global>
2021-11-23 18:39:19 +0700

    Bugfix: Create section and preview layout

--- src/common/mapping/section.js
+++ src/common/mapping/section.js
@@ -55,7 +55,8 @@ export const sectionMappingToApi = section => {
       },
       dueDate: {
         name: 'due_date',
-        parse: value => new Date(value)
+        parse: value => new Date(value),
+        isForce: true
       },
       assigneeId: {
         name: 'assigned_user_id',
